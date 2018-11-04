b454c89623f58b5338b7751c76fcf627ff022e13
Gary Cornell <gary@devcu.com>
2018-11-04 12:58:18 -0500

    New

--- src/Application.php
+++ src/Application.php
@@ -1,1 +1,79 @@
-Temp
+<?php
+/**
+ * @package		wxdashboard
+ * @author		PlanetMaster
+ * @copyright	(c) 2018 WXdisco
+ */
+ 
+namespace IPS\wxdashboard;
+
+/**
+ * WX Dashboard Application Class
+ */
+class _Application extends \IPS\Application
+{
+	/**
+	 * Init
+	 *
+	 * @return	void
+	 */
+	public function init()
+	{
+		/* If the viewing member cannot view the board (ex: guests must login first), then send a 404 Not Found header here, before the Login page shows in the dispatcher */
+		if ( !\IPS\Member::loggedIn()->group['g_wx_view_dashboard'] and \IPS\Settings::i()->wxd_enable_dash === 1 and ( \IPS\Request::i()->module == 'mydash' and \IPS\Request::i()->controller == 'index' ) )
+		{
+			\IPS\Output::i()->error( 'wxdash_no_permission', '2WX210/3', 403, '' );
+		}
+		
+		\IPS\Output::i()->cssFiles = array_merge( \IPS\Output::i()->cssFiles, \IPS\Theme::i()->css( 'wxdashboard.css' ) );
+
+		if ( \IPS\Theme::i()->settings['responsive'] )
+		{
+			\IPS\Output::i()->cssFiles = array_merge( \IPS\Output::i()->cssFiles, \IPS\Theme::i()->css( 'wxdashboard_responsive.css', 'wxdashboard', 'front' ) );
+		}
+	}
+
+	/**
+	 * [Node] Get Icon for tree
+	 *
+	 * @note	Return the class for the icon (e.g. 'globe')
+	 * @return	string|null
+	 */
+	protected function get__icon()
+	{
+		return 'cloud';
+	}
+	
+	/**
+	 * Default front navigation
+	 *
+	 * @code
+	 	
+	 	// Each item...
+	 	array(
+			'key'		=> 'Example',		// The extension key
+			'app'		=> 'core',			// [Optional] The extension application. If ommitted, uses this application	
+			'config'	=> array(...),		// [Optional] The configuration for the menu item
+			'title'		=> 'SomeLangKey',	// [Optional] If provided, the value of this language key will be copied to menu_item_X
+			'children'	=> array(...),		// [Optional] Array of child menu items for this item. Each has the same format.
+		)
+	 	
+	 	return array(
+		 	'rootTabs' 		=> array(), // These go in the top row
+		 	'browseTabs'	=> array(),	// These go under the Browse tab on a new install or when restoring the default configuraiton; or in the top row if installing the app later (when the Browse tab may not exist)
+		 	'browseTabsEnd'	=> array(),	// These go under the Browse tab after all other items on a new install or when restoring the default configuraiton; or in the top row if installing the app later (when the Browse tab may not exist)
+		 	'activityTabs'	=> array(),	// These go under the Activity tab on a new install or when restoring the default configuraiton; or in the top row if installing the app later (when the Activity tab may not exist)
+		)
+	 * @endcode
+	 * @return array
+	 */
+	public function defaultFrontNavigation()
+	{
+		return array(
+			'rootTabs'		=> array( array( 'key' => 'Wxdashboard' ) ),
+			'browseTabs'	=> array(),
+			'browseTabsEnd'	=> array(),
+			'activityTabs'	=> array()
+		);
+	}
+}
