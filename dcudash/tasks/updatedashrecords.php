558177e6e9129b7d9b04e7133b564e09913b2dcf
GaalexxC <gary@devcu.com>
2019-01-09 21:12:50 -0500

    Tasks
    Updated

--- dcudash/tasks/updatedashrecords.php
+++ dcudash/tasks/updatedashrecords.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * @brief		Update Database Task
+ * @brief		Dashboard Tasks Update Records
  * @package		DCU Dashboard customized for WX Dashboard (WX Disco)
  * @author		Gary Cornell for devCU Software Open Source Projects
- * @copyright		c) 2018 devCU Software
+ * @copyright		(c) 2019 devCU Software
  * @contact		gary@devcu.com
  * @site		https://www.devcu.com
- * @Source		https://github.com/WXdisco/wxdashboard  
- * @since		12 DEC 2018
- * @version		1.0.0 Beta 1
+ * @Source		https://github.com/devCU/DCU-Dashboard 
+ * @subpackage		Dashboard Content
+ * @base		IPS 4 CMS
+ * @since		09 JAN 2019
+ * @version		1.0.0
  */
 
 namespace IPS\dcudash\tasks;
@@ -21,7 +23,7 @@ if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
 }
 
 /**
- * updaterecords Task
+ * updatedashrecords Task
  */
 class _updaterecords extends \IPS\Task
 {
